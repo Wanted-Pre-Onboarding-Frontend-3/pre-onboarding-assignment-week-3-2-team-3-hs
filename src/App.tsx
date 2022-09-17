0461b8a4f4852a2f305af0d01ad53ceb5f03a33e
ycs1m1yk <satoly4@gmail.com>
2022-09-17 19:24:15 +0900

    feat: App UI 작성
    - CommentList
    - Form
    - PageList

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import { Counter } from "./features/counter/Counter";
+import CommentList from './features/counter/comments/CommentList';
+import Form from './features/counter/comments/Form';
+import PageList from './features/counter/comments/PageList';
 
 function App() {
   return (
     <div>
-      <header>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <CommentList />
+      <Form />
+      <PageList />
     </div>
   );
 }
